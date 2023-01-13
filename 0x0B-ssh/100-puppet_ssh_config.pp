77b798e6f2e4310b0f9dff1d419d4249447ba63d
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-01-12 18:48:50 -0800

    fix bugs

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,16 @@
 #practice puppet by making changes to ssh_config file
 file_line { 'IdentifyFile property':
-	ensure => 'present',
-	path => '/etc/ssh/ssh_config',
-	replace => 'true'
-	line => '	IdentifyFile ~/.ssh/school'
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  replace => 'true',
+  line    => '	IdentifyFile ~/.ssh/school',
+
 }
 
 file_line { 'PasswordAuthentication property':
-	ensure => 'present',
-	path => '/etc/ssh/ssh_config',
-	replace => 'true'
-	line => '	PasswordAuthentication no'
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  replace => 'true',
+  line    => '	PasswordAuthentication no',
+
 }
