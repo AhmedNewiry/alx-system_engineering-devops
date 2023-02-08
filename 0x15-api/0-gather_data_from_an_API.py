535f51d0ab188d475765f7463696fcaaed645215
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-02-08 14:15:36 -0800

    API tasks

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,20 +7,19 @@
 import requests
 import sys
 
+
 api = 'https://jsonplaceholder.typicode.com'
 if __name__ == '__main__':
-	emp_id = sys.argv[1]
-	user_info = requests.get('{}/users/{}'.format(api, emp_id))
-	user_info = user_info.json()
-	todos = requests.get('{}/todos?userId={}'.format(api, emp_id))
-	todos = todos.json()
-	completed_todos = []
-	for todo in todos:
-		if todo['completed'] == True:
-			completed_todos.append(todo)
-	print("Employee {} is done with tasks({}/{}):"
-    .format(user_info['name'], len(completed_todos), len(todos)))
+    emp_id = sys.argv[1]
+    user_info = requests.get('{}/users/{}'.format(api, emp_id))
+    user_info = user_info.json()
+    todos = requests.get('{}/todos?userId={}'.format(api, emp_id))
+    todos = todos.json()
+    completed_todos = []
+    for todo in todos:
+        if todo['completed'] is True:
+            completed_todos.append(todo)
+    print("Employee {} is done with tasks({}/{}):"
+            .format(user_info['name'], len(completed_todos), len(todos)))
     for todo in completed_todos:
-    	print("\t {}".format(todo['title']))
-
-
+        print("\t {}".format(todo['title'])
