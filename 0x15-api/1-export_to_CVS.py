2ff83637ba006b32d9b80d4c75576981ef78ac15
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-02-08 14:24:50 -0800

    API tasks

--- 0x15-api/1-export_to_CVS.py
+++ 0x15-api/1-export_to_CVS.py
@@ -10,15 +10,15 @@ import cvs
 
 api = 'https://jsonplaceholder.typicode.com'
 if __name__ == '__main__':
-	emp_id = sys.argv[1]
-	file_name = f'{emp_id}.cvs'
-	user_info = requests.get('{}/users/{}'.format(api, emp_id))
-	user_info = user_info.json()
-	todos = requests.get('{}/todos?userId={}'.format(api, emp_id))
-	todos = todos.json()
-	with open(filename, mode="w") as cvs_f:
-    	writer = csv.writer(cvs_f, delimiter=',',
+    emp_id = sys.argv[1]
+    file_name = f'{emp_id}.cvs'
+    user_info = requests.get('{}/users/{}'.format(api, emp_id))
+    user_info = user_info.json()
+    todos = requests.get('{}/todos?userId={}'.format(api, emp_id))
+    todos = todos.json()
+    with open(filename, mode="w") as cvs_f:
+        writer = csv.writer(cvs_f, delimiter=',',
                             quotechar='"', quoting=csv.QUOTE_ALL)
-        for todo in todos:
-            writer.writerow((todo.['userId'], user_info.['username'],
-                            todo.['completed'], todo.['title']))
+    for todo in todos:
+        writer.writerow((todo.['userId'], user_info.['username'],
+                         todo.['completed'], todo.['title']))
