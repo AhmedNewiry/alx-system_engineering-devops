55a904bdee7ee2a4f1c223a856b5014322dd8827
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-02-08 16:00:29 -0800

    API tasks

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -10,13 +10,13 @@ import json
 
 api = 'https://jsonplaceholder.typicode.com'
 if __name__ == '__main__':
-	emp_id = sys.argv[1]
-	file_name = f'{emp_id}.json'
-	user_info = requests.get('{}/users/{}'.format(api, emp_id))
-	user_info = user_info.json()
-	todos = requests.get('{}/todos?userId={}'.format(api, emp_id))
-	todos = todos.json()
-	emp_todos = {}
-	emp_todos[emp_id] = todos
-	with open(file_name, mode='w') as json_file:
-		json.dump(emp_todos, json_file)
+    emp_id = sys.argv[1]
+    file_name = f'{emp_id}.json'
+    user_info = requests.get('{}/users/{}'.format(api, emp_id))
+    user_info = user_info.json()
+    todos = requests.get('{}/todos?userId={}'.format(api, emp_id))
+    todos = todos.json()
+    emp_todos = {}
+    emp_todos[emp_id] = todos
+    with open(file_name, mode='w') as json_file:
+        json.dump(emp_todos, json_file)
