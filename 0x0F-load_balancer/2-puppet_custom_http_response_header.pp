2420922aa78dcc2bc2c2b6e15469461340e724b7
AhmedNewiry <93213603+AhmedNewiry@users.noreply.github.com>
2023-05-15 13:45:35 +0300

    Delete 0x0F-load_balancer directory

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++
@@ -1,8 +0,0 @@
-# creating a custom HTTP header response with Puppet.
-exec { 'custom header':
-  command  => 'apt-get -y update;
-  sudo apt-get -y install nginx;
-  sudo sed -i "/server_name _;/a add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default;
-  service nginx restart',
-  provider => shell,
-}
