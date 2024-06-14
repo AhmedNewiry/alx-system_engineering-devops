b58c2df58a330a87af7c6b2fe0f31e9f452cca7e
AhmedNewiry <ahmedabdalfttah@outlook.com>
2024-06-13 17:45:37 -0700

    user limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# update file limit for the user
+exec { 'update file limit for the user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# update the soft limit for the user
+exec { 'update soft limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
