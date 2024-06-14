e96991df75ae65e6a52888ed445ae0fb0d3fe9d3
AhmedNewiry <ahmedabdalfttah@outlook.com>
2024-06-13 17:41:21 -0700

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,12 @@
-exec { 'increase worker limit':
-  command     => 'sed -i "s/4/15000/" /etc/default/nginx && systemctl restart nginx',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
-  logoutput   => true,
-  unless      => 'grep -q "4" /etc/default/nginx',
+#update nginx soft limti
+exec { 'update soft limit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
 }
+
+#Restart nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
+
